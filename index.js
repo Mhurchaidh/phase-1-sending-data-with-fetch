5ea3ebc41df8b3e726e303ef52a4073014c23be2
mhurchaidh <bmurchie1997@gmail.com>
2022-12-20 09:38:40 -0800

    commit tree 9bdcb43914843ae948edd882b189e3b4fc4090d3

--- index.js
+++ index.js
@@ -1,1 +1,28 @@
-// Add your code here
+function submitData(name, email) {
+    
+    const configObject = {
+        method: "POST",
+        headers: {
+            "Content-Type": "application/json",
+            "Accept": "application/json",
+        },
+        body: JSON.stringify({
+            name,
+            email,
+        }),
+    }
+
+    return fetch("http://localhost:3000/users", configObject)
+        .then(resp => resp.json())
+        .then(object => {
+            //document.body.innerHTML = object.id;
+            console.log(object);
+            document.querySelector('body').append(object.id);
+        })
+        .catch(error => {
+            alert("Bad things!");
+            document.querySelector('body').append(error.message);
+            return error;
+        })
+}
+
